76821829c31c80ee560ba95779a0a83f7de5f1d3
Howarth <celagiadev@gmail.com>
2024-07-22 09:37:50 +0100

    heading test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,7 +22,7 @@
             </div>
             <h2 class="pb-4">Sounds</h2>
             <div class="d-flex justify-content-center flex-column align-items-center">
-            <h3>Wood Pigeon</h3>
+            <h3>Wood Pigeon Sound</h3>
             <audio controls autoplay muted>
           <source src="/storage/sounds/XC825469 - Common Wood Pigeon.mp3" type="audio/mpeg">
           Your browser does not support the audio element.
